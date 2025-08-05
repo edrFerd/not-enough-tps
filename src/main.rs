2f13f35fdefdde3ff9f6a9132f2f384d56c12c81
shenjackyuanjie <3695888@qq.com>
2025-08-05 13:53:22 +0800

    耳朵最爱的 debug server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
+pub mod server;
+
+use rumqttc::{MqttOptions, AsyncClient, QoS};
+use tokio::{task, time};
+use std::time::Duration;
+
+pub async fn async_main() -> anyhow::Result<()> {
     println!("Hello, world!");
+
+    let mut mqttoptions = MqttOptions::new("rumqtt-async", "test.mosquitto.org", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).await.unwrap();
+
+    task::spawn(async move {
+        for i in 0..10 {
+            client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+            time::sleep(Duration::from_millis(100)).await;
+        }
+    });
+
+    while let Ok(notification) = eventloop.poll().await {
+        println!("Received = {:?}", notification);
+    }
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let rt = tokio::runtime::Builder::new_multi_thread().enable_all().worker_threads(4).build()?;
+
+    rt.block_on(async_main())?;
+
+    Ok(())
 }
